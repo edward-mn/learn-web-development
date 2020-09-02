6b677d71730ff58fc5323a3f0e713e79396d19b0
edward-mn <edward.moreira@yahoo.com>
2020-09-02 16:17:08 -0300

    Updating my progress in the course
    JS: Events - Improving the acessibility - Done

--- udemy/curso-web-design-html-css-e-javascript/Java Script/Desafio_Menu/menu_hamburgger2/javascript/menu.js
+++ udemy/curso-web-design-html-css-e-javascript/Java Script/Desafio_Menu/menu_hamburgger2/javascript/menu.js
@@ -1,14 +1,23 @@
 (function(){
   'use strict';
-  var $mainMenu = document.querySelector('.header-nav__hamburgger');
+  var $btn = document.querySelector('.header-nav__hamburgger');
   var $htmlRoot = document.querySelector('html');
+
+  var ariaControl = $btn.getAttribute('aria-controls'); // Acessando atribuito
+
+  var $menu = document.getElementById(ariaControl); // ariaControl = '#mainMenu'
+  console.log($menu);
 
   $htmlRoot.classList.remove('no-js');
   $htmlRoot.classList.add('js');
   
-  $mainMenu.addEventListener('click', openMenu);
-
-  function openMenu(){
+  $btn.addEventListener('click', function(){
     $htmlRoot.classList.toggle('menu-opened');
-  };
+
+    // Temos que converter, pois qualquer boolean contendo string é true 
+    var ariaExpanded = this.getAttribute('aria-expanded') === "true";
+
+    this.setAttribute('aria-expanded', !ariaExpanded);
+    $menu.setAttribute('aria-expanded', !ariaExpanded);
+  });
 })();
